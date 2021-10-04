138bdce93549ff8debeb71b858f626c98ac5bb02
BBashini <b4balach@ryerson.ca>
2021-10-04 03:42:51 -0400

    25 mins changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import "./app.scss"
+
+import Topbar from "./components/topbar/Topbar"
+import Intro from "./components/intro/Intro"
+import Portfolio from "./components/portfolio/Portfolio"
+import Works from "./components/works/Works"
+import Testimonials from "./components/testimonials/Testimonials"
+import Contact from "./components/contact/Contact"
 
 function App() {
   return (
     <div className="app">
-      hello
+      <Topbar/>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Works/>
+        <Testimonials/>
+        <Contact/>
+      </div>
     </div>
   );
 }
