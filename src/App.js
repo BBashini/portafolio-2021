5ccb72fa767147e723d25596d45c0c8037507acf
BBashini <b4balach@ryerson.ca>
2021-10-04 21:43:21 -0400

    1hr changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Portfolio from "./components/portfolio/Portfolio"
 import Works from "./components/works/Works"
 import Testimonials from "./components/testimonials/Testimonials"
 import Contact from "./components/contact/Contact"
+import Menu from "./components/menu/Menu"
+
 import { useState } from "react"
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
