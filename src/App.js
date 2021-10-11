e0c7cfda5991a3bc293fec9b9495f559204f25c0
BBashini <b4balach@ryerson.ca>
2021-10-10 22:32:23 -0400

    changes to about

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Works from "./components/works/Works"
 import About from "./components/about/About"
 import Contact from "./components/contact/Contact"
 import Menu from "./components/menu/Menu"
+import Ball from "./components/ball/Ball"
 
 import { useState } from "react"
 
@@ -22,6 +23,7 @@ function App() {
         <Works/>
         <About/>
         <Contact/>
+        <Ball/>
       </div>
     </div>
   );
---
+++ src/components/ball/Ball.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./ball.scss"
+
+export default function ball() {
+    return (
+        <div className="ball-body">
+            <div id="ball"></div>
+            <div id="shadow"></div>
+        </div>
+    )
+}
+
+
